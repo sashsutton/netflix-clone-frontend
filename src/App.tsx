21ee7c04922a9fbb61842a902109f5df173fbe98
sashasutton <sashasuttons3@gmail.com>
2025-12-20 13:52:07 +0100

    add InfoModal component to App layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Navbar from "./components/Navbar.tsx"
 import Billboard from "./components/Billboard.tsx";
 import MovieList from "./components/MovieList.tsx";
+import InfoModal from "./components/InfoModal.tsx";
 
 const movies = [
     {
@@ -48,7 +49,9 @@ function App() {
           <div className="pb-40">
               <MovieList data={movies} title="Trending Now" />
               <MovieList data={movies} title="My List" />
+              <InfoModal/>
           </div>
+
       </div>
 
   )
